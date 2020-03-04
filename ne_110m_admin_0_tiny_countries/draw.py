9ed689d2363ec05ea82a0e366fa2b8e9bff89e7f
JIN <jason.jiner@gmail.com>
2020-03-04 02:59:54 +0100

    clean code

--- ne_110m_admin_0_tiny_countries/draw.py
+++
@@ -1,6 +0,0 @@
-import geopandas as gpd 
-
-shapefile = './ne_110m_admin_0_tiny_countries.shp'
-
-f = gpd.read_file(shapefile)[['ADMIN', 'ADM0_A3', 'geometry']]
-f.columns = ['country', 'country_code', 'geometry']
